d82d74cf1a330335f395460fdbc011b280f15b83
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-07-13 22:54:03 -0400

    created erc 721 contract and other boiler contracts

---
+++ contracts/MappedNFTDeployer.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./roles/Ownable.sol";
+import "./interfaces/IMappedTokenDeployer.sol";
+import "./proxy/BeaconProxy.sol";
+
+contract MappedNFTDeployer {
+    
+}
